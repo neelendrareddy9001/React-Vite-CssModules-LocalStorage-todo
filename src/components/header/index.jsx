78f6dbdf83260866b665081d36088c9856faaa8e
Neelendrareddy <31968527+neelendrareddy9001@users.noreply.github.com>
2023-02-09 23:56:58 +0100

    Update index.jsx

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,14 +1,27 @@
 import styles from './header.module.css'
 import todoLogo from '../../assets/todoLogo.svg';
 import { AiOutlinePlusCircle } from 'react-icons/ai';
+import { useState } from 'react';
 
 export function Header () {
+    const [title, setTitle] = useState('');
+    
+    function handleSubmit (event)  {
+        event.preventDefault();
+
+        onAddTask(title);
+    }
+
+    function onChangeTitle (event) {
+        setTitle(event.target.value);
+    }
+    
     return (
         <header className={styles.header}>
             <img src={todoLogo}/>
 
-            <form className={styles.newTaskForm}>
-            <input placeholder='add a new task' type='text'/>
+            <form className={styles.newTaskForm} onSubmit={handleSubmit}>
+            <input placeholder='add a new task' type='text' onChange={onChangeTitle}/>
             <button>
                 Create
                 <AiOutlinePlusCircle size={20}/>
