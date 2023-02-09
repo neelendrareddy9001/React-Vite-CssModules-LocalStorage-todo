4ef4256f04fdf3a1686ebb5f128d389986349a16
Neelendrareddy <31968527+neelendrareddy9001@users.noreply.github.com>
2023-02-09 23:58:45 +0100

    Update index.jsx

--- src/components/Task/index.jsx
+++ src/components/Task/index.jsx
@@ -1,7 +1,7 @@
 import styles from './task.module.css'
 import {TbTrash} from 'react-icons/tb'
 
-export function Task () {
+export function Task ({task}) {
     return (
         <div className={styles.task}>
             <button className={styles.checkContainer}>
@@ -9,7 +9,7 @@ export function Task () {
             </button>
 
             <p>
-                integer urma interdo libere auctor turpis turpis sapeer vel sad fames interger.
+                {task.title}
             </p>
 
             <button className={styles.deleteButton}>
