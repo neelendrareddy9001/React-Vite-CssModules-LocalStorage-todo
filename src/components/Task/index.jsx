dda4ff41517e8913d76d53445fef04cad487746d
Neelendrareddy <31968527+neelendrareddy9001@users.noreply.github.com>
2023-02-09 23:27:40 +0100

    Add files via upload

---
+++ src/components/Task/index.jsx
@@ -0,0 +1,21 @@
+import styles from './task.module.css'
+import {TbTrash} from 'react-icons/tb'
+
+export function Task () {
+    return (
+        <div className={styles.task}>
+            <button className={styles.checkContainer}>
+                <div/>
+            </button>
+
+            <p>
+                integer urma interdo libere auctor turpis turpis sapeer vel sad fames interger.
+            </p>
+
+            <button className={styles.deleteButton}>
+                <TbTrash size={20}/>
+            </button>
+        </div>
+
+    )
+}
