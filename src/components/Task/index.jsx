6d73e79a8db0189dd0b28545127e46123126080d
Neelendrareddy <31968527+neelendrareddy9001@users.noreply.github.com>
2023-02-10 23:22:58 +0100

    Update index.jsx

--- src/components/Task/index.jsx
+++ src/components/Task/index.jsx
@@ -1,7 +1,7 @@
 import styles from './task.module.css'
 import {TbTrash} from 'react-icons/tb'
 
-export function Task ({task, onComplete}) {
+export function Task ({task, onComplete, onDelete}) {
     return (
         <div className={styles.task}>
             <button className={styles.checkContainer} onClick={() => onComplete(task.id)}>
@@ -10,10 +10,10 @@ export function Task ({task, onComplete}) {
             </button>
 
             <p>
-                {task.title}
+              <p className={task.isCompleted ? styles.textCompleted : ""}>{task.title}</p>   
             </p>
 
-            <button className={styles.deleteButton}>
+            <button className={styles.deleteButton} onClick={() => onDelete(task.id)}>
                 <TbTrash size={20}/>
             </button>
         </div>
